524cfef1f9ad2246e8e78ae461a77773d135920b
Sangha Jeon <sangha082214@gmail.com>
2026-04-19 17:45:04 +0900

    Add SECRET_KEY startup check to prevent insecure default

--- app/auth.py
+++ app/auth.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise RuntimeError("SECRET_KEY environment variable is not set")
 ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"])
