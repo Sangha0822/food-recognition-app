c10704c71a0cf7bb4ed690143e913ce0eb2c6376
Sangha Jeon <sangha082214@gmail.com>
2026-05-03 23:06:48 +0900

    fixed echo to False to preserve the log in rendar

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database.db")
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
